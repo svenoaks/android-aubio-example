b085a318030a7d5aa1f75d8d9fc45c504d76acb2
Steve <svenoaks@gmail.com>
2020-08-07 10:31:14 -0400

    remove silence

--- jni/pitch.c
+++ jni/pitch.c
@@ -4,12 +4,10 @@
 #include <jni.h>
 #include "aubio.h"
 
-jfieldID getPtrFieldId(JNIEnv * env, jobject obj)
-{
+jfieldID getPtrFieldId(JNIEnv *env, jobject obj) {
     static jfieldID ptrFieldId = 0;
 
-    if (!ptrFieldId)
-    {
+    if (!ptrFieldId) {
         jclass c = (*env)->GetObjectClass(env, obj);
         ptrFieldId = (*env)->GetFieldID(env, c, "ptr", "J");
         (*env)->DeleteLocalRef(env, c);
@@ -18,12 +16,10 @@ jfieldID getPtrFieldId(JNIEnv * env, jobject obj)
     return ptrFieldId;
 }
 
-jfieldID getInputFieldId(JNIEnv * env, jobject obj)
-{
+jfieldID getInputFieldId(JNIEnv *env, jobject obj) {
     static jfieldID ptrFieldId = 0;
 
-    if (!ptrFieldId)
-    {
+    if (!ptrFieldId) {
         jclass c = (*env)->GetObjectClass(env, obj);
         ptrFieldId = (*env)->GetFieldID(env, c, "input", "J");
         (*env)->DeleteLocalRef(env, c);
@@ -32,12 +28,10 @@ jfieldID getInputFieldId(JNIEnv * env, jobject obj)
     return ptrFieldId;
 }
 
-jfieldID getTempoFieldId(JNIEnv * env, jobject obj)
-{
+jfieldID getTempoFieldId(JNIEnv *env, jobject obj) {
     static jfieldID ptrFieldId = 0;
 
-    if (!ptrFieldId)
-    {
+    if (!ptrFieldId) {
         jclass c = (*env)->GetObjectClass(env, obj);
         ptrFieldId = (*env)->GetFieldID(env, c, "tempo", "J");
         (*env)->DeleteLocalRef(env, c);
@@ -62,14 +56,15 @@ jfieldID getTempoFieldId(JNIEnv * env, jobject obj)
     (*env)->SetLongField(env, obj, getPitchFieldId(env, obj), (jlong) (pitch));
 }*/
 
-void Java_ffpetrovic_anrdroid_1aubio_1example_MainActivity_initTempo(JNIEnv * env, jobject obj, jint sampleRate, jint bufferSize)
-{
+void Java_ffpetrovic_anrdroid_1aubio_1example_MainActivity_initTempo(JNIEnv *env, jobject obj,
+                                                                     jint sampleRate,
+                                                                     jint bufferSize) {
     unsigned int win_s = (unsigned int) bufferSize; // window size
     unsigned int hop_s = win_s / 4; // hop size
     unsigned int samplerate = (unsigned int) sampleRate; // samplerate
-    aubio_tempo_t * o = new_aubio_tempo ("default", win_s, hop_s, samplerate);
-    fvec_t *input = new_fvec (hop_s); // input buffer
-    fvec_t *tempo = new_fvec (1);
+    aubio_tempo_t *o = new_aubio_tempo("default", win_s, hop_s, samplerate);
+    fvec_t *input = new_fvec(hop_s); // input buffer
+    fvec_t *tempo = new_fvec(1);
 //    aubio_tempo_set_silence(o, -10000);
 
     (*env)->SetLongField(env, obj, getPtrFieldId(env, obj), (jlong) (o));
@@ -106,32 +101,35 @@ void Java_ffpetrovic_anrdroid_1aubio_1example_MainActivity_initTempo(JNIEnv * en
     return freq;
 }*/
 
-jfloat Java_ffpetrovic_anrdroid_1aubio_1example_MainActivity_getTempo(JNIEnv * env, jobject obj, jfloatArray inputArray)
-{
-    aubio_tempo_t * o = (aubio_tempo_t *) (*env)->GetLongField(env, obj, getPtrFieldId(env, obj));
+jfloat Java_ffpetrovic_anrdroid_1aubio_1example_MainActivity_getTempo(JNIEnv *env, jobject obj,
+                                                                      jfloatArray inputArray) {
+    aubio_tempo_t *o = (aubio_tempo_t *) (*env)->GetLongField(env, obj, getPtrFieldId(env, obj));
     fvec_t *input = (fvec_t *) (*env)->GetLongField(env, obj, getInputFieldId(env, obj));
-    fvec_t *tempo = (fvec_t *) (*env)->GetLongField(env, obj, getTempoFieldId(env, obj)); // input buffer
+    fvec_t *tempo = (fvec_t *) (*env)->GetLongField(env, obj,
+                                                    getTempoFieldId(env, obj)); // input buffer
 
     jsize len = (*env)->GetArrayLength(env, inputArray);
-    if(len != input->length) {
+    if (len != input->length) {
         return 0.0f;
     }
 
     jfloat *body = (*env)->GetFloatArrayElements(env, inputArray, 0);
     // 1. copy inputArray to fvec_t* input (can be optimised)
-    for(u_int i = 0; i < len; i++) {
+    for (u_int i = 0; i < len; i++) {
         fvec_set_sample(input, body[i], i);
     }
     (*env)->ReleaseFloatArrayElements(env, inputArray, body, 0);
 
 
     float res = 0;
-    if(aubio_silence_detection(input, 45) == 0) {
-        aubio_tempo_do (o, input, tempo);
-        if (tempo->data[0] != 0) {
-            res = aubio_tempo_get_bpm(o);
-        }
+
+    aubio_tempo_do(o, input, tempo);
+    if (tempo->data[0] != 0) {
+        res = aubio_tempo_get_bpm(o);
+    } else {
+        res = 0.0f;
     }
+
     return res;
 }
 
